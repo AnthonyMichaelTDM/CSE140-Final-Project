bad5a786480c95bfd4e6cb4c9d3c66b88d7149e2
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-04-13 15:29:12 -0700

    Update ControlSignals struct and ALU signal enums

--- src/signals.rs
+++ src/signals.rs
@@ -8,11 +8,13 @@ pub struct ControlSignals {
     /// tells the register file to write to the register specified by the instruction.
     pub reg_write: Option<bool>,
     /// The branch signal is a 1 bit signal that controls whether a branch *can* be taken. (It is not the branch condition itself. That is determined by the ALU zero signal.)
-    pub branch: Option<bool>,
-    /// The ALUSrc signal is a 1 bit signal that tells the ALU whether to use the register value (0) or the immediate value (1) as the second operand.
-    pub alu_src: Option<ALUSrc>,
+    pub branch: bool,
+    /// The ALUSrcA signal is a 1 bit signal that tells the ALU whether to use the register value (0), the PC (1), or the constant 0 as the second operand.
+    pub alu_src_a: ALUSrcA,
+    /// The ALUSrcB signal is a 1 bit signal that tells the ALU whether to use the register value (0), the immediate value (1), or the constant 4 as the second operand.
+    pub alu_src_b: ALUSrcB,
     /// The ALU operation signal is a 2 bit signal that tells the ALU Control Unit what type of instruction is being executed.
-    pub alu_op: Option<ALUOp>,
+    pub alu_op: ALUOp,
     /// The mem_write signal is a 1 bit signal that tells the data memory unit whether to write to memory.
     pub mem_write: Option<bool>,
     /// controls whether the write back stages uses the output of the data memory unit or the ALU.
@@ -26,11 +28,12 @@ pub struct ControlSignals {
     pub pc_src: PCSrc,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Default)]
 #[repr(u8)]
 /// a 2 bit signal that tells the ALU Control Unit what type of instruction is being executed
 pub enum ALUOp {
     /// The ALU should perform an ADD operation, this is the case for memory load and store instructions.
+    #[default]
     ADD = 0b00,
     /// The ALU should perform a SUB operation, this is the case for branch instructions.
     SUB = 0b01,
@@ -39,25 +42,38 @@ pub enum ALUOp {
     FUNCT = 0b10,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Default)]
 #[repr(u8)]
-/// a 1 bit signal that tells the ALU whether to use the register value (0) or the immediate value (1) as the second operand.
-pub enum ALUSrc {
+/// a 1 bit signal that tells the ALU whether to use the register value (0), the PC (1), or the constant 0 as the second operand.
+pub enum ALUSrcA {
+    #[default]
     Register = 0,
-    Immediate = 1,
+    PC = 1,
+    Constant0 = 2,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Default)]
+#[repr(u8)]
+/// a 1 bit signal that tells the ALU whether to use the register value (0), the immediate value (1), or the constant 4 as the second operand.
+pub enum ALUSrcB {
+    #[default]
+    Register = 0,
+    Immediate = 1,
+    Constant4 = 2,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Default)]
 #[repr(u8)]
 /// a 4 bit signal that tells the ALU what operation to perform.
 pub enum ALUControl {
-    ADD = 0b0010,
-    SUB = 0b0110,
     AND = 0b0000,
     OR = 0b0001,
+    #[default]
+    ADD = 0b0010,
     SLL = 0b0011,
     SLT = 0b0100,
     SLTU = 0b0101,
+    SUB = 0b0110,
     XOR = 0b0111,
     SRL = 0b1000,
     SRA = 0b1010,
